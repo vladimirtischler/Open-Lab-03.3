584586042110cff640893ce8a1feccf8768cfd25
vladimirtischler <tischler.vladimir@gmail.com>
2019-11-07 08:59:32 +0100

    fgn

---
+++ Open-Lab-03.3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Open_Lab_03._3
+{
+    class Program
+    {
+        public static bool CompareCharactersCount(string a, string b)
+        {
+            return (a.Length == b.Length);
+        }
+        static void Main(string[] args)
+        {
+            string name1 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            Console.WriteLine(CompareCharactersCount(name1, name2));
+        }
+    }
+}
